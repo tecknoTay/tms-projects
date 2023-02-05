02d2b3a5bf4733ba9406dbf8a88229616e0a2fe8
teckbytay <tecknwTay@gmail.com>
2023-02-04 18:49:11 -0600

    Block letters
    First & Last name initial created in python

---
+++ ascII_block_letters.py
@@ -0,0 +1,31 @@
+#Name: Taylor Smith
+#Fun fact: I love coding
+
+#Making first initial as a block letter
+
+first_initial = """
+
+T T T T T T
+    T
+    T
+    T
+    T
+
+"""
+
+#Making second initial as a block letter
+
+second_initial = """
+
+   S S S
+S          S
+S
+   S S S   
+            S
+            S
+ S
+   S  S S           
+
+"""
+
+print(first_initial + second_initial)
